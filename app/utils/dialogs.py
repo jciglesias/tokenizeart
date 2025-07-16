71fe79c657d44e68228f5d92cd01c6bf6d054c2b
Juan carlos IGLESIAS GONZALEZ <jiglesia@f6r7s7.clusters.42paris.fr>
2025-07-16 17:52:18 +0200

    add wallet without dialog

--- app/utils/dialogs.py
+++ app/utils/dialogs.py
@@ -3,19 +3,22 @@ import utils.near as utils
 
 
 @st.dialog("create_wallet", width="large")
+def ask_create_wallet():
+    with st.form("create_wallet_form"):
+        st.text_input("Wallet Name", key="wallet_name")
+        st.text_input("Wallet Password", type="password", key="wallet_password")
+        if st.form_submit_button("Create Wallet"):
+            st.session_state.wallet = {
+                "name": st.session_state.wallet_name,
+                "password": st.session_state.wallet_password
+            }
+            utils.save_wallet(st.user["email"], st.session_state.wallet)
+            st.success("Wallet created successfully!")
+            st.rerun()
+
 def create_wallet_dialog():
     user_wallet = utils.get_wallet(st.user["email"])
     if not user_wallet:
-        with st.form("create_wallet_form"):
-            st.text_input("Wallet Name", key="wallet_name")
-            st.text_input("Wallet Password", type="password", key="wallet_password")
-            if st.form_submit_button("Create Wallet"):
-                st.session_state.wallet = {
-                    "name": st.session_state.wallet_name,
-                    "password": st.session_state.wallet_password
-                }
-                utils.save_wallet(st.user["email"], st.session_state.wallet)
-                st.success("Wallet created successfully!")
+        ask_create_wallet()
     else:
         st.session_state.wallet = user_wallet
-        st.rerun()
